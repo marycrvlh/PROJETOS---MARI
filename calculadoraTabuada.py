6f9ade272f96eeac3bf60c6a6f869ae715ba7a26
Mariane Carvalho Dearo <marycarvalhodearo@gmail.com>
2025-08-22 09:10:31 -0300

    Add files via upload

---
+++ calculadoraTabuada.py
@@ -0,0 +1,15 @@
+#solicitando as informações
+num=int(input('digite o número: '))
+
+num1=num*1
+num2=num*2
+num3=num*3
+num4=num*4
+num5=num*5
+num6=num*6
+num7=num*7
+num8=num*8
+num9=num*9
+num10=num*10
+
+print(f'{num} X 1 = {num1} \n{num} X 2 = {num2}\n{num} X 3 = {num3}\n{num} X 4 = {num4}\n{num} X 5 = {num5}\n{num} X 6 = {num6} \n{num} X 7 = {num7}\n{num} X 8 = {num8}\n{num} X 9 = {num9}\n{num} X 10 = {num10}')
