944eb184c25be59f78b0a37ab892598a067527b6
Mariane Carvalho Dearo <marycarvalhodearo@gmail.com>
2025-09-26 09:03:43 -0300

    Update listaAdiciona.py

--- listaAdiciona.py
+++ listaAdiciona.py
@@ -9,17 +9,17 @@ print(lista_compras) #mostra a lista, agora com uma nova informação dada pelo 
 
 #sofisticando e deixando o codigo mais bonito
 print('\n lista de compras: ')
-for item in lista_compras:
+for item in lista_compras: #para cada item da lista
     print(f'- {item}')
     
-#adicionando mais de um produto na lista
+#+======= adicionando mais de um produto na lista ========+
 lista_compras = []
-while True:
+while True: #enquanto a condição for verdadeira
     produto = input('digite o nome do produto (ou "sair" para encerrar): ')
     #verificando se o usuario quer sair
-    if produto.lower() == 'sair':
+    if produto.lower() == 'sair': #lower: converte todos os caracteres para minusculo
         print('encerrando o programa...')
-        break
+        break #quebra a continuidade da condição
     #adicionando um produto na lista
     lista_compras.append(produto)
     #mostrando a lista atualizada
